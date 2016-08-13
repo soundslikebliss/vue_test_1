b151ff00cde7f96c8c588a81437060e5220d513e
David <russianid1@gmail.com>
2016-08-12 22:32:35 -0700

    copy changes - about view

--- components.js
+++ components.js
@@ -1,4 +1,21 @@
 // Components
+var About = Vue.extend({
+    template: '<div class="pages-content">' +
+                    '<h2>My Deal</h2>' +
+                    '<hr>' +
+                    '<br />' +
+                    '<p>I\'\m David Adams and I\'\m a Front-End Web Developer.</p>' +
+                    '<br />' +
+                    '<p>I have experience building engaging, responsive interfaces on the web using a whole slew of web technologies.</p>' +
+                    '<br />' +
+                    '<br />' +
+                    '<p>Thanks for coming by!</p>' +
+                    '<a v-link="{ path: \'/\' }" class="hvr-shutter-in-horizontal page-button">Home</a>' +
+                '</div>'
+
+
+})
+
 var Home = Vue.extend({
     template: '#home-content'
 })
@@ -26,7 +43,7 @@ var Work = Vue.extend({
 var Skills = Vue.extend({
     template:
             '<div class="pages-content">' +
-                '<h2>Oh it\'s\ a long list... but some of the most pertinant things that I have a solid understanding of:</h2>' +
+                '<h2>Oh it\'s\ a long list... but some of the most pertinent things that I have a solid understanding of:</h2>' +
                 '<br />' +
                 '<ul><li>Wordpress (and custom fields, types)</li><li>PHP, OOP PHP</li><li>JS, AJAX</li> <li>AngularJS, VueJS</li><li>Sass</li><li>Grunt</li></ul>' +
                 '<a v-link="{ path: \'/\' }" class="hvr-shutter-in-horizontal page-button">Home</a>' +
@@ -38,23 +55,23 @@ var Projects = Vue.extend({
             '<div class="pages-content">' +
                 '<h2>Angular</h2>' +
                 '<hr>' +
-                '<a href="" class="project-link">Comic Shopper</a>' +
+                '<a href="http://comics.pizzapartypdx.com/" class="project-link" target="_blank">Comic Shopper</a>' +
                 '<h2 class="pad-top">general Front-End</h2>' +
                 '<hr>' +
-                '<a href="" class="project-link">Sans</a>' +
+                '<a href="http://sanssouci.pizzapartypdx.com/" class="project-link" target="_blank">Sans</a>' +
                 '<br />' +
-                '<a href="" class="project-link">Cinnamon</a>' +
+                '<a href="http://cinnamon.pizzapartypdx.com/" class="project-link" target="_blank">Cinnamon</a>' +
                 '<h2 class="pad-top">Ruby/Rails</h2>' +
                 '<hr>' +
-                '<a href="" class="project-link">Board</a>' +
+                '<a href="http://bloggyb.herokuapp.com/posts" class="project-link" target="_blank">Board</a>' +
                 '<br />' +
-                '<a href="" class="project-link">Travel Photos</a>' +
+                '<a href="http://ancient-sea-6769.herokuapp.com/" class="project-link" target="_blank">Travel Photos</a>' +
                 '<br />' +
                 '<a v-link="{ path: \'/\' }" class="hvr-shutter-in-horizontal page-button">Home</a>' +
             '</div>'
 })
 
-
+// instantiate app and router
 var App = Vue.extend({})
 var router = new VueRouter({
     hashbang: false
@@ -65,6 +82,9 @@ var router = new VueRouter({
 router.map({
     '/': {
         component: Home
+    },
+    '/about': {
+        component: About
     },
     '/work': {
         component: Work
