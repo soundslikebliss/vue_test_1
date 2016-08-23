3263f89c55a10a79b4de2d162a66eda088bd4554
David <russianid1@gmail.com>
2016-08-23 14:25:17 -0700

    added yoda project

--- components.js
+++ components.js
@@ -6,7 +6,7 @@ var About = Vue.extend({
                     '<br />' +
                     '<p>I\'\m David Adams and I\'\m a Front-End Web Developer.</p>' +
                     '<br />' +
-                    '<p>I have experience building engaging, responsive interfaces on the web using a whole slew of web technologies.</p>' +
+                    '<p>I have experience building engaging, responsive interfaces on the web using a whole slew of web technologies. I strive to create simple, clean, and accessible solutions for end users.</p>' +
                     '<br />' +
                     '<br />' +
                     '<p>Thanks for coming by!</p>' +
@@ -23,19 +23,29 @@ var Home = Vue.extend({
 var Work = Vue.extend({
     template:
             '<div class="pages-content">' +
-                '<h2>Jive Software (6/15 - 7/16)</h2>' +
+                '<h2>Jive Software</h2>' +
                 '<br />' +
-                '<p>Contributed to jivesoftware.com -> a heavily cusomized WP site using custom post types, custom fields, and more!</p>' +
-                '<br />' +
-                '<br />' +
-                '<h2>Empirical UX (2/15 - 6/15)</h2>' +
-                '<br />' +
-                '<p>Built UI components and interactions using AngularJS for data-intensive apps used by Intel and Jaguar. Used services and dependency injection to share data models.</p>' +
+                '<p>Worked on jivesoftware.com and other Jive properties while consistently incorporating new ideas via the Marketing department. Coded in heavily customized Wordpress builds that utilized custom post types, child themes, custom fields (ACF). Wrote PHP loops, functions, ternary expressions and conditional statements in creating templates from scratch, feeding data and generating logic. Wrote Javascript functions and data storage objects to facilitate translation of Marketo form fields to various foreign languages depending on visitor\'\s geographic location.</p>' +
                 '<br />' +
                 '<br />' +
-                '<h2>WebLively (8/14 - 2/15)</h2>' +
+                '<h2>Empirical UX</h2>' +
                 '<br />' +
-                '<p>Responsible for converting key areas of the application to responsive layouts. Designed and implemented modernizing style updates with input from stakeholders. Worked with lead dev on UX strategy.</p>' +
+                '<p>Built UI components and interactions using AngularJS for data-intensive apps used by Intel and Jaguar. Used services and dependency injection to share data models. Migrated several areas of code from jQuery to Angular. Made calls to Intel back-end codebase using http service. Used existing Angular directives to loop through data to build out UI. Created custom directives that corresponded to specific areas of the application and its respective data.</p>' +
+                '<br />' +
+                '<br />' +
+                '<h2>WebLively</h2>' +
+                '<br />' +
+                '<p>Responsible for converting landing pages to responsive, mobile-friendly designs. Used Bootstrap grid and custom media queries. Coded in Less and compiled to CSS. Designed and implemented modernizing style updates with input from stakeholders. Worked in CodeIgniter codebase writing both PHP and JS. Acted with CEO to construct overall UX strategy.</p>' +
+                '<br />' +
+                '<br />' +
+                '<h2>Unigo Group</h2>' +
+                '<br />' +
+                '<p>Co-created and maintained ​corporate blog​ following brand guidelines using Foundation 5 and Wordpress. Used Sass to add features and modify styling in Unigo.com code-base. Designed email template mock-ups in collaboration with marketing department. Gained further exposure to .Net, Visual Studio, version control, agile development, and task management tools (Jira, etc.). </p>' +
+                '<br />' +
+                '<br />' +
+                '<h2>Switchboard</h2>' +
+                '<br />' +
+                '<p>Helped to create internal metrics dashboard using front-end MVC framework with dynamic data populated through RESTful API calls. ​ Constructed Rails application to serve FAQs to users, incorporating a CMS-style interface for non-technical admins.​ Utilized Javascript to create scrolling and show/hide functionality. ​ Pushed other new site features such as ​Javascript tool-tips, site-wide alerts, ​contextual pop-ups and email preview functionality to code-base.</p>' +
                 '<a v-link="{ path: \'/\' }" class="hvr-shutter-in-horizontal page-button">Home</a>' +
              '</div>'
 })
@@ -45,7 +55,7 @@ var Skills = Vue.extend({
             '<div class="pages-content">' +
                 '<h2>Oh it\'s\ a long list... but some of the most pertinent things that I have a solid understanding of:</h2>' +
                 '<br />' +
-                '<ul><li>Wordpress (and custom fields, types)</li><li>PHP, OOP PHP</li><li>JS, AJAX</li> <li>AngularJS, VueJS</li><li>Sass</li><li>Grunt</li></ul>' +
+                '<ul><li>JS, JQuery, AJAX</li> <li>AngularJS, VueJS, currently learning ReactJS</li><li>HTML, CSS, Sass</li><li>Grunt</li><li>Wordpress (and custom fields, types, themes)</li><li>PHP, OOP PHP</li><li>UX Wireframing in Balsamiq and Gliffy</li></ul>' +
                 '<a v-link="{ path: \'/\' }" class="hvr-shutter-in-horizontal page-button">Home</a>' +
              '</div>'
 })
@@ -53,15 +63,22 @@ var Skills = Vue.extend({
 var Projects = Vue.extend({
     template:
             '<div class="pages-content">' +
-                '<h2>Angular</h2>' +
+                '<h2>AngularJS</h2>' +
+                '<h3>(single-page application, drag and drop functionality, custom directives (components))</h3>' +
                 '<hr>' +
                 '<a href="http://comics.pizzapartypdx.com/" class="project-link" target="_blank">Comic Shopper</a>' +
-                '<h2 class="pad-top">general Front-End</h2>' +
+                '<h2 class="pad-top">AJAX, OOP</h2>' +
+                '<h3>(external API consumption using AJAX, OOP with PHP)</h3>' +
+                '<hr>' +
+                '<a href="http://yoda.pizzapartypdx.com/" class="project-link" target="_blank">English to Yoda Translator</a>' +
+                '<h2 class="pad-top">General Front-End.</h2>' +
+                '<h3>(scroll-to behavior, animations, modals, image carousel, responsiveness)</h3>' +
                 '<hr>' +
                 '<a href="http://sanssouci.pizzapartypdx.com/" class="project-link" target="_blank">Sans</a>' +
                 '<br />' +
                 '<a href="http://cinnamon.pizzapartypdx.com/" class="project-link" target="_blank">Cinnamon</a>' +
-                '<h2 class="pad-top">Ruby/Rails</h2>' +
+                '<h2 class="pad-top">Back-End</h2>' +
+                '<h3>(Ruby/Rails, persistent data storage (SQL), user authentication)</h3>' +
                 '<hr>' +
                 '<a href="http://bloggyb.herokuapp.com/posts" class="project-link" target="_blank">Board</a>' +
                 '<br />' +
